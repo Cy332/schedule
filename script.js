1d8c37a60b9a7678bb9dfb64ffc9b4d972eedc53
Cy332 <bonnie.wong326@gmail.com>
2024-11-02 19:01:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+<script src="script.js">
+  $(document).ready(function() {
+    $("td").click(function() {
+        $(this).toggleClass("highlight");
+    });
+});
+$(document).ready(function() {
+    $("td").click(function() {
+        let subject = $(this).text();
+        alert("課程名稱: " + subject);
+    });
+});
+$(document).ready(function() {
+    $("#search").on("keyup", function() {
+        let searchValue = $(this).val().toLowerCase();
+        $("td").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchValue) > -1);
+        });
+    });
+});
+
+  </script>
